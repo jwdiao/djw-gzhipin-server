f8b2f4983aa174fa139e178f9f8f330016b1941e
diao jun wen <diao jun wen>
2018-09-18 23:34:59 +0800

    'updata'

--- db/db_test.js
+++ db/db_test.js
@@ -55,7 +55,7 @@ function testremove() {
   })
 }
 //testremove()*/
-============
+
 /*
 使用mongoose操作mongodb的测试文件
 1. 连接数据库
@@ -79,13 +79,13 @@ const md5 = require('blueimp-md5')
 // 1.1. 引入mongoose
 const mongoose = require('mongoose')
 // 1.2. 连接指定数据库(URL只有数据库是变化的)
-mongoose.connect('mongodb://localhost:27017/gzhipin_test4')
+mongoose.connect('mongodb://localhost:27017/gzhipin_test',{useNewUrlParser:true})
 // 1.3. 获取连接对象
-const conn = mongoose.connection
-// 1.4. 绑定连接完成的监听(用来提示连接成功)
-conn.on('connected', function () {
+ mongoose.connection.on('connected', function () {
   console.log('数据库连接成功!')
 })
+// 1.4. 绑定连接完成的监听(用来提示连接成功)
+
 
 /*2. 得到对应特定集合的Model*/
 // 2.1. 字义Schema(描述文档结构)
@@ -96,7 +96,7 @@ const userSchema = mongoose.Schema({
 })
 
 // 2.2. 定义Model(与集合对应, 可以操作集合)
-const UserModel = mongoose.model('users', userSchema)
+const UserModel = mongoose.model('UserModel', userSchema)
 
 /*3. 通过Model或其实例对集合数据进行CRUD操作*/
 // 3.1. 通过Model实例的save()添加数据
@@ -111,7 +111,7 @@ function testSave() {
   })
 }
 
-testSave()
+//testSave()
 
 // 3.2. 通过Model的find()/findOne()查询多个或一个数据
 function testFind() {
